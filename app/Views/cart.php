2c694c5d37195bc46721f1ff1902ba852028da01
Jason Napolitano <jason-napolitano@users.noreply.github.com>
2019-10-11 01:02:57 -0700

    Update cart.php

--- app/Views/cart.php
+++ app/Views/cart.php
@@ -1,47 +1,51 @@
-<section style="width: 965px">
-    <table cellpadding="6" cellspacing="1" style="width:100%" border="0">
+<section class="container">
+    <div class="row">
+        <div class="col">
+            <table class="table table-bordered table-striped">
 
-        <tr>
-            <th>QTY</th>
-            <th>Item Description</th>
-            <th style="text-align:right">Item Price</th>
-            <th style="text-align:right">Sub-Total</th>
-        </tr>
-        <?php $i = 1; ?>
-        <?php foreach ($cart->contents() as $items): ?>
-            <?php echo form_hidden($i.'[rowid]', $items['rowid']); ?>
-            <tr>
-                <td><?php echo form_input(array('name' => $i.'[qty]', 'value' => $items['qty'], 'maxlength' => '3', 'size' => '5')); ?></td>
-                <td>
-                    <?php echo $items['name']; ?>
+                <tr>
+                    <th>QTY</th>
+                    <th>Item Description</th>
+                    <th style="text-align:right">Item Price</th>
+                    <th style="text-align:right">Sub-Total</th>
+                </tr>
+                <?php $i = 1; ?>
+                <?php foreach ( $cart->contents() as $items ): ?>
+                    <tr>
+                        <td>
+                            <?= $items[ 'qty' ]; ?>
+                        </td>
+                        <td>
+                            <?php echo $items[ 'name' ]; ?>
 
-                    <?php if ($cart->hasOptions($items['rowid']) == TRUE): ?>
+                            <?php if ( $cart->hasOptions($items[ 'rowid' ]) === true ): ?>
 
-                        <p>
-                            <?php foreach ($cart->productOptions($items['rowid']) as $option_name => $option_value): ?>
+                                <p>
+                                    <?php foreach ( $cart->productOptions($items[ 'rowid' ]) as $option_name => $option_value ): ?>
 
-                                <strong><?php echo $option_name; ?>:</strong> <?php echo $option_value; ?><br />
+                                        <strong><?php echo $option_name; ?>:</strong> <?php echo $option_value; ?><br/>
 
-                            <?php endforeach; ?>
-                        </p>
+                                    <?php endforeach; ?>
+                                </p>
 
-                    <?php endif; ?>
+                            <?php endif; ?>
 
-                </td>
-                <td style="text-align:right"><?php echo $cart->formatNumber($items['price']); ?></td>
-                <td style="text-align:right">$<?php echo $cart->formatNumber($items['subtotal']); ?></td>
-            </tr>
+                        </td>
+                        <td style="text-align:right"><?php echo $cart->formatNumber($items[ 'price' ]); ?></td>
+                        <td style="text-align:right">$<?php echo $cart->formatNumber($items[ 'subtotal' ]); ?></td>
+                    </tr>
 
-            <?php $i++; ?>
+                    <?php $i++; ?>
 
-        <?php endforeach; ?>
+                <?php endforeach; ?>
 
-        <tr>
-            <td colspan="2"> </td>
-            <td class="right"><strong>Total</strong></td>
-            <td class="right">$<?php echo $cart->formatNumber($cart->total()); ?></td>
-        </tr>
+                <tr>
+                    <td colspan="2"></td>
+                    <td class="right"><strong>Total</strong></td>
+                    <td class="right">$<?php echo $cart->formatNumber($cart->total()); ?></td>
+                </tr>
 
-    </table>
-
+            </table>
+        </div>
+    </div>
 </section>
